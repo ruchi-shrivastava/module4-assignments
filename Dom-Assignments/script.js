366ed8cf2b312d0f48bbc0741fa4a897f83ac6fc
Ruchi Shrivastava <39464621+ruchi-shrivastava@users.noreply.github.com>
2024-07-27 20:16:32 +0530

    new commit

---
+++ Dom-Assignments/script.js
@@ -0,0 +1,11 @@
+const result = document.getElementById("square");
+console.log(result);
+result.style.backgroundColor = "yellow";
+result.style.margin = "20px";
+result.style.padding = "10px";
+result.style.fontSize = '18px';
+result.style.fontWeight = "bold";
+result.style.height = "200px";
+result.style.width = "300px";
+result.style.color = "blue";
+
