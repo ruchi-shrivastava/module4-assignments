abd5083c2e0403d34c7af5b9a4800dacf97bfae5
Ruchi Shrivastava <39464621+ruchi-shrivastava@users.noreply.github.com>
2024-08-11 16:24:39 +0530

    stopwath added

--- feedback-UI/script.js
+++ feedback-UI/script.js
@@ -1,42 +1,22 @@
-// const ratingEls = document.querySelectorAll(".rating");
-// const btnEl = document.getElementById("btn");
-
-// const containerEl = document.getElementById("container");
-
-// let selectedRating = "";
-
-// ratingEls.forEach((ratingEl) => {
-//   ratingEl.addEventListener("click", (event) => {
-//     removeActive();
-//     selectedRating =
-//       event.target.innerText || event.target.parentNode.innerText;
-//     event.target.classList.add("active");
-//     event.target.parentNode.classList.add("active");
-//   });
-// });
-
-// btnEl.addEventListener("click", () => {
-//   if (selectedRating !== "") {
-//     containerEl.innerHTML = `
-//         <strong>Thank you!</strong>
-//         <br>
-//         <br>
-//         <strong>Feedback: ${selectedRating}</strong>
-//         <p>We'll use your feedback to improve our customer support.</p>
-//         `;
-//   }
-// });
-
-// function removeActive() {
-//   ratingEls.forEach((ratingEl) => {
-//     ratingEl.classList.remove("active");
-//   });
-// }
 
 let containerVar = document.getElementById("container");
 let ratingContainerVar = document.querySelectorAll(".rating");
 let buttonVar = document.getElementById("btn");
 console.log(containerVar,ratingContainerVar,btn);
+
+
+//Handling Click Events on Rating Elements
+// i am using for loop for understanding purpose
+
+//for (let i = 0; i < ratingContainerVar.length; i++) {
+    //ratingContainerVar[i].addEventListener("click", function(event) {
+      //  removeActive();
+       // selectedRating = event.target.innerText || event.target.parentNode.innerText;
+      //  event.target.parentNode.classList.add("active");
+   // });
+//}
+
+
 
 let selectedRating = "";
 
